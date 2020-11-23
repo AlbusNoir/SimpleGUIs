196704cbcabc69db4056741939989b4d15497780
Kale <13501991+AlbusNoir@users.noreply.github.com>
2020-11-23 18:58:56 -0500

    uploading new simpletodo

--- Random_Non_Proj_GUIS/simpletodo.py
+++ Random_Non_Proj_GUIS/simpletodo.py
@@ -1,20 +1,35 @@
 # simple to-do list to practice pysimplegui
 import PySimpleGUI as sg
 
-rangenum = int(input('How many items should we create? '))
 
-def todoitem(num):
-    return [sg.Text(f'{num}. '), sg.CBox(''), sg.In()]
+items = []
+
+run = True
+while run:
+    print('Enter item or Q to quit\n')
+    item = input('Enter item for todo list: ')
+    if item not in ['q', 'Q']:
+        items.append(item)
+    else:
+        run = False
 
 
-layout = [todoitem(x) for x in range(1, rangenum+1)] + [[sg.B('Save'), sg.B('Exit')]]
+total = len(items)
+
+
+def todoitem(item):
+    for x in range(1, total):
+        return [sg.CBox(''), sg.In(item)]
+        
+
+layout = [todoitem(item) for item in items] + [[sg.B('Save'), sg.B('Exit')]]
 
 window = sg.Window('Simple ToDo', layout)
 event, values = window.read()
 
 
 
-# TODO add rangenum as a window
+# TODO add input as window
 # TODO add menus for changing theme
 # TODO add functionality to save
 # TODO add functionality to the checkboxes
