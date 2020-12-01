c2c10b06839729b28f8455ab42778b2ebce410af
Kale <13501991+AlbusNoir@users.noreply.github.com>
2020-12-01 16:22:27 -0500

    added save functionality to todolist

--- Random_Non_Proj_GUIS/todolist.py
+++ Random_Non_Proj_GUIS/todolist.py
@@ -1,38 +1,61 @@
-# TODO: how the heck do save - find out and add save button
 # TODO: make uuuh prettier
 # TODO: add a menubar?
 
 
 import PySimpleGUI as sg
+import os
+from datetime import date
 
 layout = [
     [sg.T('', size=(8, 1), key='-DATE-'), sg.CalendarButton('Set Date'), sg.I(key='-TASK-', size=(30, 1))],
-    [sg.B('Add', key='-ADD-'), sg.Cancel()],
+    [sg.B('Add Task', key='-ADD-'), sg.B('Save', key='-SAVE-'), sg.B('Exit', key='-EXIT-')],
 ]
 
 window = sg.Window('Todo', layout)
 
 def create_task(tasks):
     layout = [
-        [sg.T('', size = (8, 1), key = '-DATE-'), sg.CalendarButton('Set Date'), sg.I(key = '-TASK-', size = (30, 1))]
+        [sg.T('', size = (8, 1), key='-DATE-'), sg.CalendarButton('Set Date'), sg.I(key='-TASK-', size = (30, 1))]
     ]
     # dynamically add to layout: taskid, task, delete button for each task in tasks
-    layout += [[sg.T(idx, size = (3, 1)), sg.T(t, size = (30, 1), key = f'-TX-{idx}'), sg.T('x', key = f'-DEL-{idx}',
+    layout += [[sg.T(idx, size = (3, 1)), sg.T(t, size = (30, 1), key=f'-TX-{idx}'), sg.T('x', key=f'-DEL-{idx}',
                                                                                             enable_events = True)] for
                idx, t in enumerate(tasks, start = 1)]
     # add buttons
-    layout += [[sg.B('Add', key = '-ADD-'), sg.Cancel()]]
+    layout += [[sg.B('Add Task', key='-ADD-'), sg.B('Save', key='-SAVE-'), sg.B('Exit', key='-EXIT-')]]
     # make new window, close existing, make new window main window
     window1 = sg.Window('Todo', layout)
     
     return window1
 
 
+def save_file(filename):
+    if not os.path.exists(filename):
+        if filename not in (None, ''):
+            with open(filename, 'w') as f:
+                for task in tasks:
+                    f.write(task+'\n')
+    else:
+        file_save_as()
+
+
+def file_save_as():
+    filename = sg.popup_get_file('Save File', save_as=True, no_window=True, file_types=(('Text Files', '*.txt'),))
+    if filename not in (None, '') and not isinstance(filename, tuple):
+        with open(filename, 'w') as f:
+            for task in tasks:
+                f.write(task+'\n')
+    return filename
+
+
 tasks = []
+
+today = date.today()
+cur_date = today.strftime('%d%b%Y')
 
 while True:
     event, values = window.read()
-    if event in ['Cancel', sg.WIN_CLOSED]:
+    if event in ['-EXIT-', sg.WIN_CLOSED]:
         break
     if event == '-ADD-':
         # get value in -TASK-, get value of -DATE-, split on space, grab index 0
@@ -51,3 +74,10 @@ while True:
         window1 = create_task(tasks)
         window.close()
         window = window1
+
+    elif event == '-SAVE-':
+        filename = f'{cur_date}.txt'
+        save_file(filename)
+        sg.popup('File saved')
+    elif event in ('Save As',):
+        filename = file_save_as()
